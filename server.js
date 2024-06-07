18ff171661a1d938229767271d367f26e2bc536d
kasem1234357 <kasemalolo2002@gmail.com>
2024-06-07 15:34:07 +0300

    updated request

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ dotenv.config();
 
 const { connectDB } = require('./db/connectDB');
 // handle unknown variable error
-process.on('uncaughtException', (err) => {
-    console.log(err.name, err.message);
-    console.log('Uncaught Exception occured! Shutting down...');
-    process.exit(1);
- })
+// process.on('uncaughtException', (err) => {
+//     console.log(err.name, err.message);
+//     console.log('Uncaught Exception occured! Shutting down...');
+//     process.exit(1);
+//  })
 
 const app = require('./app');
 
@@ -18,13 +18,13 @@ const server = app.listen(port, () => {
     console.log('server has started...');
 })
 // handling connect to DB error
-process.on('unhandledRejection', (err) => {
-   console.log(err.name, err.message);
-   console.log('Unhandled rejection occured! Shutting down...');
+// process.on('unhandledRejection', (err) => {
+//    console.log(err.name, err.message);
+//    console.log('Unhandled rejection occured! Shutting down...');
 
-   server.close(() => {
-    process.exit(1);
-   })
-})
+//    server.close(() => {
+//     process.exit(1);
+//    })
+// })
 
 
